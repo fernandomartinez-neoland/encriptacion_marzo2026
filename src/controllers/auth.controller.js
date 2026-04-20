354323c79646189ac14f87b2a37a90f2fb7508ac
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-20 14:01:43 +0200

    bcrypt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { registerService } from '../services/auth.service.js';
 
 export async function registerController(req, res) {
+    console.log("hola")
   const result = await registerService(req.body);
   res.json(result);
 }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,12 @@
-import  userModel  from '../models/user.model.js';
+import userModel from '../models/user.model.js';
+import bcrypt from 'bcrypt'
+
+const saltRounds = 10
 
 export async function registerService(userData) {
-  const user = await userModel();
-  return user.find({});
+    console.log(userData)
+    const hashedpass = bcrypt.hash(userData.password, saltRounds);
+    console.log("clave hasheada: ", await hashedpass)
+    const user = await userModel();
+    return user.find({});
 }
