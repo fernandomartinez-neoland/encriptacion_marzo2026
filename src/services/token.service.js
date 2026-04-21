70e0b4cc084bc20272a9f020815507c55b6de53d
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-21 17:32:58 +0200

    validateToken

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,5 +1,15 @@
 import jwt from 'jsonwebtoken'
 export function createToken(userInfo) {
-    const token=jwt.sign(userInfo, process.env.tokenKey)
+    const token = jwt.sign(userInfo, process.env.tokenKey)
     return token
+}
+
+export function validateToken(token) {
+    try {
+        var decoded = jwt.verify(token, process.env.tokenKey);
+
+        return decoded
+    } catch (e) {
+        return false
+    }
 }
