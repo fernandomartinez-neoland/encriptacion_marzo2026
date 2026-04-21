1d6e7a67523429deb3f52dacdc8add427bf39747
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-21 15:29:55 +0200

    tokenkey

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -1,6 +1,5 @@
 import jwt from 'jsonwebtoken'
 export function createToken(userInfo) {
-    console.log("este es el servicio de token: ",userInfo)
-    const token=jwt.sign(userInfo, '123')
+    const token=jwt.sign(userInfo, process.env.tokenKey)
     return token
 }
