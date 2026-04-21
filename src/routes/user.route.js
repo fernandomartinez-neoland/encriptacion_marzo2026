41769390e7a95d0bdb7a8cdd08645d30debdfe78
fernandomartinez-neoland <fernando.martinez@neoland.es>
2026-04-21 13:45:14 +0200

    acmodar importacion del router

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { registerController } from '../controllers/auth.controller.js';
-import { loginController } from '../controllers/auth.controller.js';
+import { registerController, loginController } from '../controllers/auth.controller.js';
+
 
 const router = express.Router();
 
